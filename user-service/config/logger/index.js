beb4550117ab50ba41f738f2a405eacfa86ba9e3
akinmaurice <akinmaurice@gmail.com>
2021-07-30 21:46:07 +0100

    Update logger

--- user-service/config/logger/index.js
+++ user-service/config/logger/index.js
@@ -7,38 +7,45 @@ const prettyJson = winston.format.printf(info => {
     timestamp = moment(timestamp).format('YYYY-MM-DD H:mm:ss:SSS');
     const { level } = info;
     const { message } = info;
-    return `[${timestamp}] ${level}: ${message}`;
+    return `[${timestamp}]: ${level}: ${message}`;
 });
 
-const combineFormat = winston.format.combine(
+const consoleCombineFormat = winston.format.combine(
     winston.format.colorize(),
     winston.format.timestamp(),
     prettyJson
 );
 
+const jsonCombineFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+);
+
 
 const logger = () => {
-    let ret = winston.createLogger({
-                    format: combineFormat,
-                    transports: [
-                        new winston.transports.Console({
-                            level: 'debug',
-                            handleExceptions: true,
-                            json: false,
-                            colorize: true
-                        }),
-                        new winston.transports.File({
-                            level: 'debug',
-                            filename: './server.log',
-                            handleExceptions: true,
-                            json: true,
-                            maxsize: 5242880,
-                            maxFiles: 100,
-                            colorize: true
-                        })
-                    ],
-                    exitOnError: false
-                });
+    const ret = winston.createLogger({
+        defaultMeta: { service: 'user-service' },
+        transports: [
+            new winston.transports.Console({
+                format: consoleCombineFormat,
+                level: 'debug',
+                handleExceptions: true,
+                json: false,
+                colorize: true
+            }),
+            new winston.transports.File({
+                format: jsonCombineFormat,
+                level: 'debug',
+                filename: './server.log',
+                handleExceptions: true,
+                json: true,
+                maxsize: 5242880,
+                maxFiles: 100,
+                colorize: true
+            })
+        ],
+        exitOnError: false
+    });
 
     ret.stream = {
         write: (message) => {
